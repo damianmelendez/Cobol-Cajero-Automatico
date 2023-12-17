2eb5c79f8e363e324a43b59afa917dead445cb3f
damian <damianmelendez5678@gmail.com>
2023-12-17 17:56:20 -0600

    cajero basico

--- Cajero-Automatico.cbl
+++ Cajero-Automatico.cbl
@@ -5,13 +5,55 @@
       * Tectonics: cobc
       ******************************************************************
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. YOUR-PROGRAM-NAME.
+       PROGRAM-ID. CAJERO-AUTOMATICO.
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
+       01  SALDO PIC 999V99 VALUE ZERO.
+       01  DEPOCITO PIC 999V99 VALUE ZERO.
+       01  RETIRO PIC 999V99.
+       01  OPCION PIC 9.
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
-            DISPLAY "Hello world"
-            STOP RUN.
-       END PROGRAM YOUR-PROGRAM-NAME.
+            DISPLAY "Bienvenido al Simulador de Cajero Automático".
+                DISPLAY "1. Consulta de Saldo".
+                DISPLAY "2. Depósito".
+                DISPLAY "3. Retiro".
+                DISPLAY "4. Salir".
+                ACCEPT OPCION.
 
+                PERFORM OPCIONES-CAJERO
+            STOP RUN.
+
+           OPCIONES-CAJERO.
+           EVALUATE OPCION
+               WHEN 1
+                PERFORM CONSULTAR-SALDO
+               WHEN 2
+                PERFORM REALIZAR-DEPOSITO
+               WHEN 3
+                PERFORM REALIZAR-RETIRO
+               WHEN 4
+                DISPLAY "Gracias por usar el Simulador de Cajero "
+               WHEN OTHER
+                DISPLAY "Opción no válida. Intente nuevamente".
+
+           CONSULTAR-SALDO.
+               DISPLAY "Saldo actual: $" SALDO.
+
+           REALIZAR-DEPOSITO.
+               DISPLAY "Ingrese la cantidad a depositar: "
+               ACCEPT DEPOCITO
+               ADD DEPOCITO TO SALDO.
+
+           REALIZAR-RETIRO.
+               DISPLAY "Ingrese la cantidad a retirar: "
+               ACCEPT RETIRO
+                   IF RETIRO > SALDO
+                       DISPLAY "Fondos insuficientes."
+                   ELSE
+                       SUBTRACT RETIRO FROM SALDO
+                       DISPLAY "Retiro exitoso. Saldo actual: $" SALDO.
+
+
+       END PROGRAM CAJERO-AUTOMATICO.
